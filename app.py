3aeb4f088f496ce0a99e2315cd169a3e4e518918
Kenny David <jp3193666@gmail.com>
2025-02-17 20:20:45 +0100

    Atualização do backend

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# Configurar CORS para aceitar requisições do seu frontend
+CORS(app, resources={r"/*": {"origins": "https://jokestop-4cc4f.web.app"}})
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    print("Dados recebidos:", data)
+    return jsonify({"message": "Cadastro recebido com sucesso!"}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
